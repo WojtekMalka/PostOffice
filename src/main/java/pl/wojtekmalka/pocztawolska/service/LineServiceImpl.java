e42372a0696e461c9ab7d160f3490bf5c8d91008
WojtekMalka <malka_wojciech@wp.pl>
2021-11-04 22:31:44 +0100

    MVP LineServiceImpl

--- src/main/java/pl/wojtekmalka/pocztawolska/service/LineServiceImpl.java
+++ src/main/java/pl/wojtekmalka/pocztawolska/service/LineServiceImpl.java
@@ -2,16 +2,19 @@ package pl.wojtekmalka.pocztawolska.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.wojtekmalka.pocztawolska.dictionary.ClientSpecialStatus;
 import pl.wojtekmalka.pocztawolska.entity.Line;
 import pl.wojtekmalka.pocztawolska.entity.Ticket;
 import pl.wojtekmalka.pocztawolska.repository.LineRepository;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LineServiceImpl {
+    public static final long LINE_ID = 1L;
     private final LineRepository lineRepository;
 
     public Line getLine(Long lineId) {
@@ -19,25 +22,33 @@ public class LineServiceImpl {
                 .orElseThrow(() -> new NoSuchElementException());
     }
 
-    private void addTicket(Line line, Ticket ticket) {
-        List<Ticket> lineToUpdate = line.getLine();
-        lineToUpdate.add(findTicketPlaceInLine(ticket),ticket);
-
-        Line.builder()
-                .line(lineToUpdate)
-                .build();
+    public void addTicket(Ticket ticket) {
+        Line line = getLine(LINE_ID);
+        line.setLine(findTicketPlaceInLine(line.getLine(), ticket));
+        lineRepository.save(line);
     }
 
-    private Integer findTicketPlaceInLine(Ticket ticket){
-        if (ticket.getClient().getSpecialStatus() != null) {
+    private List<Ticket> findTicketPlaceInLine(List<Ticket> line, Ticket ticket) {
+        checkStatusVIP(line, ticket);
+        checkStatusURGENT(line, ticket);
+        return line;
+    }
 
+    private void checkStatusURGENT(List<Ticket> line, Ticket ticket) {
+        if (ticket.getClient().getSpecialStatus().equals(ClientSpecialStatus.URGENT)
+                && ticket.getClient().getAuthCode().equals(ClientSpecialStatus.URGENT.getStatusAuthCode())) {
+            line.add(0, ticket);
         }
-        return 1;
     }
 
-//    private void updateQueue() {
-//        List<Ticket> ticket = ticketRepository.findAll().stream()
-//                .sorted(Comparator.comparingInt(Ticket::getClientPlaceInLine))
-//                .collect(Collectors.toList());
-//    }
+    private void checkStatusVIP(List<Ticket> line, Ticket ticket) {
+        if (ticket.getClient().getSpecialStatus().equals(ClientSpecialStatus.VIP)
+                && ticket.getClient().getAuthCode().equals(ClientSpecialStatus.VIP)) {
+            List<Ticket> tempLine = line.stream()
+                    .filter(s -> ClientSpecialStatus.VIP.equals(s.getClient().getSpecialStatus()))
+                    .sorted((o1, o2) -> o1.getAudit().getCreatedOn().compareTo(o2.getAudit().getCreatedOn()))
+                    .collect(Collectors.toList());
+            line.addAll(0, line);
+        }
+    }
 }
